8ff9e0f347109d80894e87d503e70da27d2d7234
Harshavardhana <harsha@minio.io>
2021-11-25 10:26:04 -0800

    fix cross platform dependencies

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+// Copyright (c) 2021 MinIO, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -19,18 +34,17 @@ import (
 	"strings"
 	"sync"
 	"sync/atomic"
-	"syscall"
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/minio/cli"
-	"github.com/minio/pkg/console"
-	"github.com/minio/pkg/ellipses"
 	"github.com/rs/dnscache"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh/terminal"
 	"golang.org/x/net/http2"
-	"golang.org/x/sys/unix"
+
+	"github.com/minio/cli"
+	"github.com/minio/pkg/console"
+	"github.com/minio/pkg/ellipses"
 )
 
 // Use e.g.: go build -ldflags "-X main.version=v1.0.0"
@@ -488,33 +502,6 @@ func dialContextWithDNSCache(resolver *dnscache.Resolver, baseDialCtx DialContex
 
 var dnsCache = &dnscache.Resolver{
 	Timeout: 5 * time.Second,
-}
-
-func setTCPParameters(network, address string, c syscall.RawConn) error {
-	c.Control(func(fdPtr uintptr) {
-		// got socket file descriptor to set parameters.
-		fd := int(fdPtr)
-
-		_ = unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
-
-		_ = unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
-
-		// Enable TCP open
-		// https://lwn.net/Articles/508865/ - 16k queue size.
-		_ = syscall.SetsockoptInt(fd, syscall.SOL_TCP, unix.TCP_FASTOPEN, 16*1024)
-
-		// Enable TCP fast connect
-		// TCPFastOpenConnect sets the underlying socket to use
-		// the TCP fast open connect. This feature is supported
-		// since Linux 4.11.
-		_ = syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, unix.TCP_FASTOPEN_CONNECT, 1)
-
-		// Enable TCP quick ACK, John Nagle says
-		// "Set TCP_QUICKACK. If you find a case where that makes things worse, let me know."
-		_ = syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, unix.TCP_QUICKACK, 1)
-
-	})
-	return nil
 }
 
 // DialContext is a function to make custom Dial for internode communications
---
+++ main_linux.go
@@ -0,0 +1,51 @@
+// +build linux
+
+// Copyright (c) 2021 MinIO, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+func setTCPParameters(network, address string, c syscall.RawConn) error {
+	c.Control(func(fdPtr uintptr) {
+		// got socket file descriptor to set parameters.
+		fd := int(fdPtr)
+
+		_ = unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+
+		_ = unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+
+		// Enable TCP open
+		// https://lwn.net/Articles/508865/ - 16k queue size.
+		_ = syscall.SetsockoptInt(fd, syscall.SOL_TCP, unix.TCP_FASTOPEN, 16*1024)
+
+		// Enable TCP fast connect
+		// TCPFastOpenConnect sets the underlying socket to use
+		// the TCP fast open connect. This feature is supported
+		// since Linux 4.11.
+		_ = syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, unix.TCP_FASTOPEN_CONNECT, 1)
+
+		// Enable TCP quick ACK, John Nagle says
+		// "Set TCP_QUICKACK. If you find a case where that makes things worse, let me know."
+		_ = syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, unix.TCP_QUICKACK, 1)
+
+	})
+	return nil
+}
